9e9255ebf7b0a06d9f6cf44ebfebd07af23dbe61
roxy_huang <sixin_huang@outlook.com>
2018-04-11 13:35:36 +0800

    update log config

--- middleware/createServer.ts
+++ middleware/createServer.ts
@@ -1,15 +1,18 @@
 import * as Koa from 'koa';
 import * as kcors from 'kcors';
 import * as bodyParser from 'koa-bodyparser';
-import * as logger from 'koa-logger';
 import * as config from 'config';
 import { router } from './createRouter';
+
+const log4js = require('log4js');
 
 
 export default async(): Promise<any> => {
     const env = config.get('env');
 
     console.log(env);
+
+    console.log(log4js);
 
     const app = new Koa();
 
